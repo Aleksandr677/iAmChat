3bb702867c096504adf4682120af37b69558a160
AleksandrKhristichenko <alexanderhristichenko@yandex.ru>
2025-09-02 17:17:21 +0300

    Initial Commit

---
+++ iAmChat/iAmChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAmChatApp.swift
+//  iAmChat
+//
+//  Created by Aleksandr Khristichenko on 02.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct iAmChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iAmChatTests/iAmChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  iAmChatTests.swift
+//  iAmChatTests
+//
+//  Created by Aleksandr Khristichenko on 02.09.2025.
+//
+
+import Testing
+
+struct iAmChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
