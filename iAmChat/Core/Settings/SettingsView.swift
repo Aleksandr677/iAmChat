ce40bab0269933c8877db3d6386883beb2744c01
AleksandrKhristichenko <alexanderhristichenko@yandex.ru>
2025-09-03 11:35:45 +0300

    (Feature): fix logout animation

--- iAmChat/Core/Settings/SettingsView.swift
+++ iAmChat/Core/Settings/SettingsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     
+    @Environment(\.dismiss) private var dismiss
     @Environment(AppState.self) private var appState
     
     var body: some View {
@@ -26,7 +27,11 @@ struct SettingsView: View {
     }
     
     func onSingOutPressed() {
-        appState.updateViewState(showTabBarView: false)
+        dismiss()
+        Task {
+            try? await Task.sleep(for: .seconds(0.5))
+            appState.updateViewState(showTabBarView: false)
+        }
     }
 }
 
