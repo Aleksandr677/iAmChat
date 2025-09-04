4a1f6e67734448848c82cf579d739901dd7f109c
AleksandrKhristichenko <alexanderhristichenko@yandex.ru>
2025-09-04 17:55:13 +0300

    (Feature): category cell

--- iAmChat/Core/Explore/ExploreView.swift
+++ iAmChat/Core/Explore/ExploreView.swift
@@ -10,16 +10,56 @@ import SwiftUI
 struct ExploreView: View {
     
     let avatar = AvatarModel.mock
+    @State private var featuredAvatars: [AvatarModel] = AvatarModel.mocks
+    @State private var categories: [CharacterOption] = CharacterOption.allCases
     
     var body: some View {
         NavigationStack {
-            HeroCellView(
-                title: avatar.name,
-                subtitle: avatar.characterDescription,
-                image: avatar.profileImageName
-            )
-                .navigationTitle("Explore")
+            List(content: {
+                featuredSection
+                categorySection
+            })
+            .navigationTitle("Explore")
         }
+    }
+    
+    private var featuredSection: some View {
+        Section(content: {
+            ZStack(content: {
+                CarouselView(items: featuredAvatars) { item in
+                    HeroCellView(title: item.name,
+                                 subtitle: item.characterDescription,
+                                 image: item.profileImageName)
+                }
+            })
+            .removeListRowFormatting()
+        },
+                header: {
+            Text("Featured Avatars")
+        })
+    }
+    
+    private var categorySection: some View {
+        Section(content: {
+            ZStack(content: {
+                ScrollView(.horizontal) {
+                    HStack(spacing: 12) {
+                        ForEach(categories, id: \.self) { category in
+                            CategoryCellView(title: category.rawValue.capitalized,
+                                             imageName: Constants.randomImageURL)
+                                .frame(width: 150)
+                        }
+                    }
+                }
+                .frame(height: 140)
+                .scrollIndicators(.hidden)
+                .scrollTargetBehavior(.viewAligned)
+            })
+            .removeListRowFormatting()
+        },
+                header: {
+            Text("Categories")
+        })
     }
 }
 
